fdab051af2e6edf35f304ebffb373690fd685d68
allen <yuyl_allen@outlook.com>
2022-07-29 20:54:34 +0800

    明显的透视！

--- learningOpenGL/src/main.cpp
+++ learningOpenGL/src/main.cpp
@@ -107,16 +107,16 @@ int main()
     };
     // world space positions of our cubes
     glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(0.0f,  0.0f,  -500.0f),
         glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-1.5f, -2.2f, -20.5f),
         glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(2.4f, -0.4f, -30.5f),
         glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.3f, -2.0f, -9.5f),
         glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
+        glm::vec3(1.5f,  0.2f, -70.5f),
+        glm::vec3(-1.3f,  1.0f, -100.5f)
     };
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
