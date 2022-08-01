8c56be46660e205a2cb1170178e219d0390c9bf9
allen <yuyl_allen@outlook.com>
2022-08-01 17:36:19 +0800

    练习3：更多箱子，更多旋转！

--- learningOpenGL/src/main.cpp
+++ learningOpenGL/src/main.cpp
@@ -234,6 +234,8 @@ int main()
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
             model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+            if (i % 3 == 0)
+                model = glm::rotate(model, 20 * (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 0.0f));
             ourShader.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
